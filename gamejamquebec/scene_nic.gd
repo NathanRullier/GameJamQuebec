4c57b983cfbcee09dfdc82c47b46cbd257a4ba45
Squirenykk <nicdupuis@outlook.com>
2024-10-05 13:04:53 -0400

    Intégration switch room 2 jean

--- gamejamquebec/scene_nic.gd
+++ gamejamquebec/scene_nic.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 var show = false;
-@onready var HeroPOV =$"Hero POV"
-@onready var HistoPOV =$"Histo POV"
+@onready var HeroPOV =$"Niveau 2/Hero POV"
+@onready var HistoPOV =$"Niveau 2/Histo POV"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	HeroPOV.visible = false
