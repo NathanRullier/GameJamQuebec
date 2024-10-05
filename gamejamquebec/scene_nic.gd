0a101ad292ad53af70c15c7bf51781d548e1b3c5
JeanJeanGSC <jeancouturier.24@gmail.com>
2024-10-05 19:40:08 -0400

    scene nic = ready to play with !

--- gamejamquebec/scene_nic.gd
+++ gamejamquebec/scene_nic.gd
@@ -1,5 +1,4 @@
 extends Node2D
-
 var show = false;
 @onready var HeroPOV =$"Niveau 2/Hero POV"
 @onready var HistoPOV =$"Niveau 2/Histo POV"
@@ -7,8 +6,6 @@ var show = false;
 func _ready() -> void:
 	HeroPOV.visible = false
 	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
