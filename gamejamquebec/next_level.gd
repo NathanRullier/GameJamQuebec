cb3aa66a52ec79e951ddb0fda2bf8a52e8077d8d
Squirenykk <nicdupuis@outlook.com>
2024-10-05 23:26:34 -0400

    Création niveau 2

--- gamejamquebec/next_level.gd
+++ gamejamquebec/next_level.gd
@@ -6,7 +6,7 @@ func _on_body_entered(body: Node2D) -> void:
 		var current_scene_file = get_tree().current_scene.scene_file_path
 		var next_level_number = current_scene_file.to_int() + 1
 		
-		var next_level_path = "res://test_map_" + str(next_level_number) + ".tscn"
+		var next_level_path = "res://Level/Level_" + str(next_level_number) + ".tscn"
 		TransitionScreen.transition()
 		await TransitionScreen.on_transition_finished
 		get_tree().change_scene_to_file(next_level_path)
